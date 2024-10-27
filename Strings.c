36b41937301cf9e757a8086f55ee4d4ec628ceff
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-10-27 21:34:54 +0530

    Add files via upload

---
+++ Strings.c
@@ -0,0 +1,12 @@
+//program to print the string
+#include<stdio.h>
+int main()
+{
+ char message[] = "How are you doing,";
+ char fname[] = "Aman ";
+
+printf("%s %s!", message, fname);
+
+  return 0;
+
+}
